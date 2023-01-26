357d80b65f63c7ad7ba32c1d57f3c43ec09ea314
leticea <nascimento.leticeah@gmail.com>
2023-01-26 16:54:18 -0300

    pegando as rotas de cada habito e recebendo no insomnia

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "./lib/prisma";
@@ -10,5 +11,63 @@ export async function appRoutes(app: FastifyInstance) {
     });
 
     const { title, weekDays } = createHabitBody.parse(request.body);
+
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: new Date(),
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
+  });
+
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+
+    const parseDate = dayjs(date).startOf("day");
+    const weekDay = parseDate.get("day");
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parseDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
   });
 }
